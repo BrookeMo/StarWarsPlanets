9476c805b44848ad752568042daac3e4844b0441
Brooke Mortensen <brookeamymo@gmail.com>
2018-06-04 19:20:51 -0600

    attempt for fixing mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,7 @@
-var height = window.innerHeight;
-    var a = setInterval(function() {
-        $(window).scrollTop(-1);
-        resize();
-    }, 500); // Don't lower more than 500ms, otherwise there will be animation-problems with the  Safari toolbar
-
-    $(window).on('resize', function() {
-        resize();
-    });
-
-    var resize = function() {
-        if(window.innerHeight != height) {
-            height = window.innerHeight;
-            $('.section').css('height', height + 'px');
-        }
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+    
+if (isSafari = true) {
+        $('#map').css("bottom", "5px")
     };
 
 $('#space').mousemove(function (e) {
