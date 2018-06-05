6b4629512cb749a2e9f8c6dcb2d0ce878b1c0678
Brooke Mortensen <brookeamymo@gmail.com>
2018-06-04 19:38:33 -0600

    ...

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
 $(document).ready(function () {
     if (isSafari = true) {
-        $('#map').css("bottom", "100px")
+        $('#map').css("top", "-50px")
     }
 });
 
