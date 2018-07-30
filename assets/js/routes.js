cafa814ac6e2793563a92ea97222c2057ac58a7f
Brooke Mortensen <brookeamymo@gmail.com>
2018-07-30 11:55:45 -0600

    layer 4-6 planet pages created

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -25,3 +25,57 @@ $("#mimban, #mimbanlabel").click(function(){
 $("#vandor, #vandorlabel").click(function(){
     location.href = "./Planets/vandor.html";
 });
+$("#shill, #shilllabel").click(function(){
+    location.href = "./Planets/shill.html";
+});
+$("#kashyyk, #kashyyklabel").click(function(){
+    location.href = "./Planets/kashyyk.html";
+});
+$("#dathomir, #dathomirlabel").click(function(){
+    location.href = "./Planets/dathomir.html";
+});
+$("#krownest, #krownestlabel").click(function(){
+    location.href = "./Planets/krownest.html";
+});
+$("#concorddawn, #concorddawnlabel").click(function(){
+    location.href = "./Planets/concorddawn.html";
+});
+$("#mandalore, #mandalorelabel").click(function(){
+    location.href = "./Planets/mandalore.html";
+});
+$("#wobani, #wobanilabel").click(function(){
+    location.href = "./Planets/wobani.html";
+});
+$("#naboo, #naboolabel").click(function(){
+    location.href = "./Planets/naboo.html";
+});
+$("#jedha, #jedhalabel").click(function(){
+    location.href = "./Planets/jedha.html";
+});
+$("#agamar, #agamarlabel").click(function(){
+    location.href = "./Planets/agamar.html";
+});
+$("#scariff, #scarifflabel").click(function(){
+    location.href = "./Planets/scariff.html";
+});
+$("#tatooine, #tatooinelabel").click(function(){
+    location.href = "./Planets/tatooine.html";
+});
+$("#genosis, #genosislabel").click(function(){
+    location.href = "./Planets/genosis.html";
+});
+$("#dqar, #dqarlabel").click(function(){
+    location.href = "./Planets/dqar.html";
+});
+$("#crait, #craitlabel").click(function(){
+    location.href = "./Planets/crait.html";
+});
+$("#hoth, #hothlabel").click(function(){
+    location.href = "./Planets/hoth.html";
+});
+$("#bespin, #bespinlabel").click(function(){
+    location.href = "./Planets/bespin.html";
+});
+$("#endor, #endorlabel").click(function(){
+    location.href = "./Planets/moonofendor.html";
+});
